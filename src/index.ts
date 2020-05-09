40a98d91bbf776cb56dbe026f01d6110eb8e9990
Jose Quintana <joseluisquintana20@gmail.com>
2020-05-09 13:29:10 +0200

    style: update project code styling

--- src/index.ts
+++ src/index.ts
@@ -5,42 +5,42 @@
  * @returns a formatted string
  */
 function sprintfv (args: any[]): string {
-  const argsLen: number = args.length
-  const formatv: string = (args[0] || '').toString()
-  const argv: any[] = []
-  const argvLen: number = argsLen > 0 ? argsLen - 1 : 0
+    const argsLen = args.length
+    const formatv: string = (args[0] || "").toString()
+    const argv: any[] = []
+    const argvLen = argsLen > 0 ? argsLen - 1 : 0
 
-  // Notice: type specifier '%s' is only supported
-  const formats: string[] = formatv.split('%s')
-  const formatsLen: number = formats.length
+  // Note: type specifier '%s' is only supported
+    const formats: string[] = formatv.split("%s")
+    const formatsLen = formats.length
 
-  if (formatsLen <= 0) {
-    return formatv
-  }
-
-  const formatLen: number = formatsLen - 1
-
-  if (formatLen > argvLen) {
-    throw new Error('Too few arguments supplied.')
-  }
-
-  for (let i = 0; i < argsLen - 1; ++i) {
-    argv.push(
-      (args[i + 1] || '').toString()
-    )
-  }
-
-  let str = ''
-
-  for (let i = 0; i < formatsLen; i++) {
-    str += formats[i] || ''
-
-    if (i < formatLen) {
-      str += argv[i] || ''
+    if (formatsLen <= 0) {
+        return formatv
     }
-  }
 
-  return str
+    const formatLen = formatsLen - 1
+
+    if (formatLen > argvLen) {
+        throw new Error("Too few arguments supplied.")
+    }
+
+    for (let i = 0; i < argsLen - 1; ++i) {
+        argv.push(
+            (args[i + 1] || "").toString()
+        )
+    }
+
+    let str = ""
+
+    for (let i = 0; i < formatsLen; i++) {
+        str += formats[i] || ""
+
+        if (i < formatLen) {
+            str += argv[i] || ""
+        }
+    }
+
+    return str
 }
 
 /**
@@ -50,7 +50,7 @@ function sprintfv (args: any[]): string {
  * @returns a formatted string
  */
 export function sprintf (...args: any[]): string {
-  return sprintfv(args)
+    return sprintfv(args)
 }
 
 /**
@@ -60,8 +60,8 @@ export function sprintf (...args: any[]): string {
  * @param argsv an array of arguments
  * @returns a formatted string
  */
-export function vsprintf (format: any = '', argsv: any[] = []): string {
-  const args = [ format ]
-  Array.prototype.push.apply(args, argsv)
-  return sprintfv(args)
+export function vsprintf (format: any = "", argsv: any[] = []): string {
+    const args = [ format ]
+    Array.prototype.push.apply(args, argsv)
+    return sprintfv(args)
 }
